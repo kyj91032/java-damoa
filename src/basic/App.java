da94c7264b2a33eb4e3762cdcbcb0685d130152d
hhee4455 <127376832+hhee4455@users.noreply.github.com>
2023-05-16 18:02:33 +0900

    ㅁㅁㅁ

--- src/basic/App.java
+++ src/basic/App.java
@@ -3,13 +3,6 @@ package basic;
 import javax.swing.JFrame;
 
 public class App extends JFrame{
-
-	
-	
-	
-	
-	
-	
 	
 	public static void main(String[] args) {
 		System.out.println("ㅋ");
--- src/basic/Home.java
+++ src/basic/Home.java
@@ -10,8 +10,9 @@ import javax.swing.JLayeredPane;
 import java.awt.CardLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import javax.swing.JButton;
 
-public class Login extends JFrame {
+public class Home extends JFrame {
 
 	/**
 	 * Launch the application.
@@ -20,7 +21,7 @@ public class Login extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login frame = new Login();
+					Home frame = new Home();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -32,27 +33,46 @@ public class Login extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Login() {
+	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400, 570);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 128, 128));
+		panel_1.setBackground(new Color(255, 255, 255));
 		panel_1.setBounds(0, 0, 386, 43);
 		getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(12, 10, 50, 15);
+		panel_1.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(233, 8, 24, 19);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(269, 10, 24, 15);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_1_1 = new JButton("New button");
+		btnNewButton_1_1.setBounds(305, 10, 24, 15);
+		panel_1.add(btnNewButton_1_1);
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(0, 64, 0));
+		panel_2.setBackground(new Color(255, 255, 255));
 		panel_2.setBounds(0, 42, 386, 428);
 		getContentPane().add(panel_2);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 128));
+		panel.setBackground(new Color(255, 255, 255));
 		panel.setBounds(0, 471, 386, 62);
 		getContentPane().add(panel);
 		setVisible(true);
+		
+		
 		
 	}
 }
