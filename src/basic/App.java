4bc679c5124ca8ae94cb737456e75394f42ec68e
김대현 <132973917+wjdrjs00@users.noreply.github.com>
2023-05-16 18:19:37 +0900

    첫 화면 완성

--- src/basic/App.java
+++ src/basic/App.java
@@ -1,11 +1,11 @@
 package basic;
 
-import java.awt.BorderLayout;
+import java.awt.BorderLayout; 
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Image;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -21,22 +21,28 @@ public class App extends JFrame{
 			setLocationRelativeTo(null);
 			getContentPane().setLayout(new BorderLayout());
 			
+			
+			
 			// contentpane의 배경 흰색으로 변경 
 			getContentPane().setBackground(Color.white);
-
 			
 			ImageIcon image = new ImageIcon("image/damoa.jpg");
 			Image img = image.getImage();
 	        
 	        // 추출된 Image의 크기 조절하여 새로운 Image 객체 생성
-	    	Image updateImg = img.getScaledInstance(120, 140, Image.SCALE_SMOOTH);
+	    	Image updateImg = img.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
 	        
 	        // 새로운 Image 객체로 ImageIcon 객체 생성
 	        ImageIcon updateIcon = new ImageIcon(updateImg);
 	        
 	        JLabel imgLabel = new JLabel(updateIcon);
-	        
-			getContentPane().add(imgLabel);
+			getContentPane().add(imgLabel, BorderLayout.CENTER);
+			
+			JLabel damoaTitle = new JLabel("d a m o a",(int) CENTER_ALIGNMENT);
+			damoaTitle.setBorder(BorderFactory.createEmptyBorder(25, 0, 0 , 0));
+			Font damoaTitleFont = new Font("궁서",Font.ITALIC,40);
+			damoaTitle.setFont(damoaTitleFont);
+			getContentPane().add(damoaTitle, BorderLayout.NORTH);
 			
 			
 			setVisible(true);
