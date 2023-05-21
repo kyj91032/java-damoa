6a1394df3ed8ad099a2bc819632e622f02384637
sees77770 <sees77770@gmail.com>
2023-05-22 02:09:22 +0900

    app.java에서 db 최초 연결, login 일부 구현

--- src/basic/App.java
+++ src/basic/App.java
@@ -4,6 +4,11 @@ import java.awt.CardLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,11 +19,15 @@ public class App extends JFrame {
     private JPanel contentPane;
     private CardLayout cardLayout;
 	private Timer timer;
+	
+	private static Connection conn;
+	private static Statement stmt;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
+                	initDbConn(); // db 연결
                     App frame = new App();
                     frame.setVisible(true);
                 } catch (Exception e) {
@@ -27,6 +36,27 @@ public class App extends JFrame {
             }
         });
     }
+    
+    private static void initDbConn() { // db 최초 연결 메소드
+    	try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/damoa",
+					"root", 
+					"1234");
+			stmt = conn.createStatement();
+			System.out.println("OK");
+						
+		} catch (ClassNotFoundException e) {
+			System.out.println("해당 드라이버가 존재하지 않습니다.");
+			e.printStackTrace();
+			
+		} catch (SQLException e) {
+			System.out.println("SQL 오류 입니다.");
+			e.printStackTrace();
+			
+		}
+	}
+
 
     public App() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,7 +72,7 @@ public class App extends JFrame {
 
         Start card1 = new Start();
         Home card2 = new Home(this);
-        Login card3 = new Login();
+        Login card3 = new Login(stmt);
 
         contentPane.add(card1, "Card1");
         contentPane.add(card2, "Card2");
@@ -62,7 +92,7 @@ public class App extends JFrame {
 
     }
     
-    public void showCard(String cardName) {
+    public void showCard(String cardName) { // 화면 전환 메소드
         cardLayout.show(contentPane, cardName);
     }
    
--- src/basic/Login.java
+++ src/basic/Login.java
@@ -9,44 +9,36 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Login extends JPanel {
+	
 	private JTextField textField;
 	private JPasswordField passwordField;
+	
+	private Statement stmt;
 
-	/**
-	 * Create the panel.
-	 */
-	public Login() {
+	
+	public Login(Statement stmt) {
+		this.stmt = stmt;
+		
 		setPreferredSize(new Dimension(400, 570));
 		setBackground(new Color(255, 255, 255));
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("아이디");
-		lblNewLabel.setBounds(49, 160, 57, 15);
-		add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("비밀번호");
-		lblNewLabel_1.setBounds(49, 250, 57, 15);
-		add(lblNewLabel_1);
+		showLoginView();
+			
 		
-		textField = new JTextField();
-		textField.setBounds(49, 185, 290, 30);
-		add(textField);
-		textField.setColumns(10);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(49, 275, 290, 30);
-		add(passwordField);
-		
-		JButton btnNewButton = new JButton("로그인");
-		btnNewButton.setBounds(242, 334, 97, 23);
-		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("회원가입");
-		btnNewButton_1.setBounds(242, 367, 97, 23);
-		add(btnNewButton_1);
-		
+		showBtnPanel();
+			
+	}
+
+	private void showBtnPanel() {
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		panel.setBounds(0, 500, 400, 70);
@@ -68,7 +60,65 @@ public class Login extends JPanel {
 		JButton btnNewButton_5 = new JButton("마이페이지");
 		btnNewButton_5.setBackground(Color.WHITE);
 		panel.add(btnNewButton_5);
+	}
+
+	private void showLoginView() {
+		JLabel lblNewLabel = new JLabel("아이디");
+		lblNewLabel.setBounds(49, 160, 57, 15);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("비밀번호");
+		lblNewLabel_1.setBounds(49, 250, 57, 15);
+		add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(49, 185, 290, 30);
+		add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(49, 275, 290, 30);
+		add(passwordField);
+		
+		JButton btnNewButton = new JButton("로그인");
+		btnNewButton.setBounds(242, 334, 97, 23);
+		add(btnNewButton);
+		
+		btnNewButton.addActionListener(new ActionListener() { // 로그인 버튼 누를 시 로그인 시도
+		    public void actionPerformed(ActionEvent e) {
+		        String username = textField.getText(); // 아이디 저장
+		        String password = new String(passwordField.getPassword()); // 비번 저장
+		        
+		        // 여기에서 DB와 비교하여 로그인 처리를 수행하는 로직을 구현
+		        if (checkLogin(username, password)) {
+		            // 로그인 성공 시 처리할 코드를 작성 예정
+		            System.out.println("로그인 성공!");
+		        } else {
+		            // 로그인 실패 시 처리할 코드를 작성 예정
+		            System.out.println("로그인 실패!");
+		        }
+		    }
+		});
+
+		
+		JButton btnNewButton_1 = new JButton("회원가입");
+		btnNewButton_1.setBounds(242, 367, 97, 23);
+		add(btnNewButton_1);
+	}
+	
+	private boolean checkLogin(String username, String password) {
+	    // DB와의 비교 로직을 구현
+		try {
+			String query = "SELECT * FROM usertable WHERE userid = '" + username + "' AND userpw = '" + password + "'";
+			ResultSet resultSet = stmt.executeQuery(query);
+			return resultSet.next();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
 		
 		
 	}
+
 }
